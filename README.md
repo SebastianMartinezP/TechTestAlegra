07689e574e7b676fed9ef606136ff7320c388115
SebastianMartinezP <sebix3180@gmail.com>
2024-02-22 21:15:55 -0300

    ADD: data warehouse ETLs

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
 # TechTestAlegra
 Technical test for Data Engineer position at Alegra.
 
-ENTREGABLES:
- - Documento de diseño de Modelo dimensional:
-     - Diagramas
-     - Descripción de dimensiones y hechos
-     - Granularidad de datos
-     - Justificación del diseño
- - Script ETL pseudolenguaje
- - .CSV muestras:
-     - archivos de cada tabla del modelo dimensional.
-     - (incluir columnas y datos ficticios)
- - responder preguntas:
-     - ¿Cuál es el producto más vendido en cada trimestre del año?
-     - ¿Cuáles son las tendencias de compra de los clientes más leales?
-     - ¿Cómo varían las ventas según las regiones geográficas durante el año?
-     - Presentar ejemplos de consultas SQL que responderían preguntas de negocio complejas utilizando el modelo propuesto
+## ENTREGABLES
+### Documento de diseño del modelo relacional
+[Documentación de diseño del Modelo Dimensional](https://docs.google.com/document/d/1xN2t9K9dk5b721JqX1mwrwtG0MGDUqq2D4DK0ZH4rYI/edit?usp=sharing).
+ 
+### Scripts ETL:
+ - `src/stagging_tables.py`: contiene la lógica de la capa stagging, mencionada en el documento.
+ - muestras de csv: 
+    - `docs/output_stagging/*.csv`
+    - `docs/output_datawarehouse/*.csv`
+
+### Responder preguntas:
++ ¿Cuál es el producto más vendido en cada trimestre del año?
+    ```sql
+    /* bigquery dialect */
+    SELECT
+        COUNT(DISTINCT i.invoice_id) as invoice_quantity,
+        p.product_name,
+        t.quarter,
+        t.year
+    FROM `dw_prod.fact_invoices` AS i
+    INNER JOIN `dw_prod.time_dim` AS t      ON i.time_id = t.id
+    INNER JOIN `dw_prod.products_dim` AS p  ON i.product_id = p.id
+    GROUP BY
+        p.product_name,
+        t.quarter,
+        t.year
+    ORDER BY
+        invoice_quantity DESC;
+    ```
+    **Respuesta**: ...
+
++ ¿Cuáles son las tendencias de compra de los clientes más leales?
+    ```sql
+    SELECT product_id, SUM(quantity) AS total_sales FROM sales GROUP BY product
+    ```
+    **Respuesta**: ...
+
++ ¿Cómo varían las ventas según las regiones geográficas durante el año?
+    ```sql
+    SELECT product_id, SUM(quantity) AS total_sales FROM sales GROUP BY product
+    ```
+    **Respuesta**: ...
+
---
+++ src/data_warehouse_tables.py
@@ -0,0 +1,335 @@
+# pip install pandas
+
+import pandas
+import random
+import string
+from typing import Union, List, Any
+
+
+
+def get_data(csv_path:str) -> Union[pandas.DataFrame, None]:
+    """ Generic function for reading data from csv file. """
+    try:
+        data = pandas.read_csv(csv_path)
+        return data
+    except Exception as e:
+        print('get_data >>>', str(e))
+        return None
+
+def save_data(dataframe:pandas.DataFrame, csv_path:str) -> bool:
+    """ Generic function for reading data from csv file. """
+    try:
+        dataframe.to_csv(
+            path_or_buf = csv_path,
+            index = False,
+            encoding = 'utf-8'
+        )
+        return True
+    except Exception as e:
+        print('save_data >>>', str(e))
+        return False
+
+def get_mock_phone_number() -> str:
+    """ Returns a mock phone number similar to the mexican format. """
+    return ''.join([
+        '+52',
+        ''.join(random.sample(string.digits, 10))
+    ])
+
+def get_mock_email() -> str:
+    """ Returns a mock email adress. """
+    return ''.join([
+        ''.join(random.sample(string.ascii_letters, 10)),
+        '@',
+        ''.join(random.sample(string.ascii_lowercase, 6)),
+        '.com'
+    ])
+
+class ETL:
+    """ Generic definitios for an ETL procedure with Pandas. """
+
+    input_csv_path: Union[str,None] = None
+    output_csv_path: Union[str,None] = None
+    data: Union[pandas.DataFrame, None] = None
+
+    def __init__(self, input_csv_path, output_csv_path) -> bool:
+        self.input_csv_path = input_csv_path
+        self.output_csv_path = output_csv_path
+        return (
+            True 
+            if isinstance(self.input_csv_path, str)
+            and isinstance(self.output_csv_path, str)
+            else False
+        )
+    
+    def extract(self) -> bool:
+        self.data = get_data(csv_path = self.input_csv_path)
+        return True if isinstance(self.data, pandas.DataFrame) else False
+    
+    # defined inside implementation eventually.
+    def transform(self) -> bool: ...
+
+    def load(self) -> bool:
+        result = save_data(
+            dataframe = self.data,
+            csv_path = self.output_csv_path
+        )
+        return result
+
+
+# translation tables
+INVALID_VOCALS:dict[int,int] = str.maketrans(
+    'áéíóúÁÉÍÓÚäëïöüÄËÏÖÜà',
+    'aeiouAEIOUaeiouAEIOUa'
+)
+
+
+
+class Customer(ETL):
+    def __init__(self, input_csv_path, output_csv_path) -> bool:
+        super().__init__(input_csv_path, output_csv_path)
+
+    def extract(self) -> None: 
+        result = super().extract()
+        print('Customer.extract > ', str(result))
+
+    def transform(self) -> None:
+        try:
+            df = self.data.copy()
+            
+            df['customer_id'] = df['ID']
+            df['segment_name'] = df['Segmento']
+            
+            # cleaning invalid vocals
+            df['name'] = df['Nombre'].map(lambda x : x.translate(INVALID_VOCALS))
+            df['location_name'] = df['Ubicacion'].map(lambda x : x.translate(INVALID_VOCALS))
+            
+            # simulating a phone number and email input
+            df['phone_number'] = df['ID'].map(lambda x : get_mock_phone_number())
+            df['email'] = df['ID'].map(lambda x : get_mock_email())
+            
+            # selecting only clean fields
+            df = df[[
+                'customer_id', 'name', 'location_name',
+                'segment_name', 'phone_number', 'email'
+            ]]
+            
+            self.data = df
+            print('Customer.transform > True')
+        except Exception as e:
+            print('Customer.transform > ERROR: ', str(e))
+
+    def load(self) -> None: 
+        result = super().load()
+        print('Customer.load > ', str(result))
+
+
+class Product(ETL):
+    def __init__(self, input_csv_path, output_csv_path) -> bool:
+        super().__init__(input_csv_path, output_csv_path)
+
+    def extract(self) -> None: 
+        result = super().extract()
+        print('Product.extract > ', str(result))
+
+    def transform(self) -> None:
+        try:
+            df = self.data.copy()
+            
+            df['product_id'] = df['ID']
+            df['name'] = df['Nombre']
+            df['price'] = df['Precio']
+
+            # cleaning invalid vocals
+            df['category'] = df['Categoria'].map(
+                lambda x : x.translate(INVALID_VOCALS)
+            )
+            
+            # simulating currency type
+            df['currency_type'] = df['ID'].map(lambda x : "MXN")
+            
+            # selecting only clean fields
+            df = df[[
+                'product_id', 'name',
+                'price', 'category',
+                'currency_type'
+            ]]
+            
+            self.data = df
+            print('Product.transform > True')
+        except Exception as e:
+            print('Product.transform > ERROR: ', str(e))
+
+    def load(self) -> None: 
+        result = super().load()
+        print('Product.load > ', str(result))
+
+
+class Time(ETL):
+    def __init__(self, input_csv_path, output_csv_path) -> bool:
+        super().__init__(input_csv_path, output_csv_path)
+
+    def extract(self) -> None: 
+        result = super().extract()
+        print('Time.extract > ', str(result))
+
+    def transform(self) -> None:
+        try:
+            df = self.data.copy()
+            
+            df['product_id'] = df['ID']
+            df['name'] = df['Nombre']
+            df['price'] = df['Precio']
+
+            # cleaning invalid vocals
+            df['category'] = df['Categoria'].map(
+                lambda x : x.translate(INVALID_VOCALS)
+            )
+            
+            # simulating currency type
+            df['currency_type'] = df['ID'].map(lambda x : "MXN")
+            
+            # selecting only clean fields
+            df = df[[
+                'product_id', 'name',
+                'price', 'category',
+                'currency_type'
+            ]]
+            
+            self.data = df
+            print('Time.transform > True')
+        except Exception as e:
+            print('Time.transform > ERROR: ', str(e))
+
+    def load(self) -> None: 
+        result = super().load()
+        print('Time.load > ', str(result))
+
+
+class PaymentMethod(ETL):
+    def __init__(self, input_csv_path, output_csv_path) -> bool:
+        super().__init__(input_csv_path, output_csv_path)
+
+    def extract(self) -> None: 
+        result = super().extract()
+        print('PaymentMethod.extract > ', str(result))
+
+    def transform(self) -> None:
+        try:
+            df = self.data.copy()
+        
+            df['invoice_id'] = df['ID']
+            df['invoice_date'] = df['Fecha']
+            df['client_id'] = df['ClienteID']
+            df['product_id'] = df['ProductoID']
+            df['total_invoice'] = df['Total']
+
+            # applying tranformations
+            df['product_quantity'] = df['Cantidad'].fillna(0)
+
+            # simulating currency type
+            df['currency_type'] = df['ID'].map(lambda x : "MXN")
+
+            # selecting only clean fields
+            df = df[[
+                'invoice_id', 'invoice_date',
+                'product_quantity',
+                'total_invoice',
+                'currency_type',
+                'client_id', 'product_id'
+            ]]
+            
+            self.data = df
+            print('PaymentMethod.transform > True')
+        except Exception as e:
+            print('PaymentMethod.transform > ERROR: ', str(e))
+
+    def load(self) -> None: 
+        result = super().load()
+        print('PaymentMethod.load > ', str(result))
+
+
+class Invoice(ETL):
+    def __init__(self, input_csv_path, output_csv_path) -> bool:
+        super().__init__(input_csv_path, output_csv_path)
+
+    def extract(self) -> None: 
+        result = super().extract()
+        print('Invoice.extract > ', str(result))
+
+    def transform(self) -> None:
+        try:
+            df = self.data.copy()
+        
+            df['invoice_id'] = df['ID']
+            df['invoice_date'] = df['Fecha']
+            df['client_id'] = df['ClienteID']
+            df['product_id'] = df['ProductoID']
+            df['total_invoice'] = df['Total']
+
+            # applying tranformations
+            df['product_quantity'] = df['Cantidad'].fillna(0)
+
+            # simulating currency type
+            df['currency_type'] = df['ID'].map(lambda x : "MXN")
+
+            # selecting only clean fields
+            df = df[[
+                'invoice_id', 'invoice_date',
+                'product_quantity',
+                'total_invoice',
+                'currency_type',
+                'client_id', 'product_id'
+            ]]
+            
+            self.data = df
+            print('Invoice.transform > True')
+        except Exception as e:
+            print('Invoice.transform > ERROR: ', str(e))
+
+    def load(self) -> None: 
+        result = super().load()
+        print('Invoice.load > ', str(result))
+
+
+if __name__ == "__main__":
+
+    customers:Customer  = Customer(
+        input_csv_path  = "docs/output_stagging/customers.csv",
+        output_csv_path = "docs/output_data_warehouse/customers_dim.csv"
+    )
+    customers.extract()
+    customers.transform()
+    customers.load()
+
+    products:Product  = Product(
+        input_csv_path  = "docs/output_stagging/products.csv",
+        output_csv_path = "docs/output_data_warehouse/products_dim.csv"
+    )
+    products.extract()
+    products.transform()
+    products.load()
+
+    time:Time  = Time(
+        input_csv_path  = "docs/output_stagging/invoices.csv",
+        output_csv_path = "docs/output_data_warehouse/time_dim.csv"
+    )
+    time.extract()
+    time.transform()
+    time.load()
+
+    # payment_method:PaymentMethod  = PaymentMethod(
+    #     input_csv_path  = "docs/output_stagging/payment_method.csv",
+    #     output_csv_path = "docs/output_data_warehouse/payment_method_dim.csv"
+    # )
+    # payment_method.extract()
+    # payment_method.transform()
+    # payment_method.load()
+
+    invoices:Invoice  = Invoice(
+        input_csv_path  = "docs/output_stagging/invoices.csv",
+        output_csv_path = "docs/output_data_warehouse/fact_invoices.csv"
+    )
+    invoices.extract()
+    invoices.transform()
+    invoices.load()
